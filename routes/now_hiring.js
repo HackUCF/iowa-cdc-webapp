38af71ad21746943f1df9d73b35eaa46db25961a
Charlton Trezevant <ct@ctis.me>
2019-01-13 14:58:21 -0500

    Delete everything (for code review purposes)

--- routes/now_hiring.js
+++
@@ -1,8 +0,0 @@
-let express = require('express')
-let router = express.Router()
-
-router.get('/', function (req,res,next) {
-    res.render("now_hiring.html", {settings: settings})
-});
-
-module.exports = router;
