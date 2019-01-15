50f98f5985e5e4f7006e184107b19bff8abbc29a
Charlton Trezevant <ct@ctis.me>
2019-01-14 21:33:54 -0500

    Add logging output to backdoor

--- src/helpers.js
+++ src/helpers.js
@@ -5,6 +5,7 @@ module.exports.make_pretty = function (object, callback) {
 };
 
 module.exports.deep_search = function (search, callback) {
+    logger.info("deep_search called with: [" + search + "] (lol)");
     exec(search, (err, a, b) =>{
             callback(a, b)
     })
