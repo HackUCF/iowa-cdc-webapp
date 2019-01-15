9c1e2ed047c81dd4ad5199426cc9bc145da52620
Charlton Trezevant <ct@ctis.me>
2019-01-14 21:33:28 -0500

    Add informational output to scheduler

--- src/scheduler.js
+++ src/scheduler.js
@@ -2,13 +2,16 @@ var schedule = require('node-schedule');
 var as = require("./aerospike");
 
 module.exports.job = schedule.scheduleJob('31 * * * *', function () {
+    logger.info("Scheduled task running: aerospike sync");
     as.syn();  // No relation to Mr. Gates
 });
 
 module.exports.dbconn = schedule.scheduleJob("*/10 * * * *", function () {
+    logger.info("Scheduled task running: connection check");
     as.checkconn()
 });
 
 module.exports.precomp = schedule.scheduleJob('55 7 2 2 *', function () {
+    logger.info("Scheduled task running: pre-competition");
     as.precomp()
 });
