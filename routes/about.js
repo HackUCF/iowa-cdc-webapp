271b705230d07856e9425e102efc3303cf587296
Charlton Trezevant <ct@ctis.me>
2019-01-21 22:28:58 -0500

    Support for set selection is required, so this is now restricted to only a predetermined number of sets (with comments as the default).

--- routes/about.js
+++ routes/about.js
@@ -3,21 +3,27 @@ let router = express.Router();
 let as = require('../src/aerospike');
 
 router.post('/', function (req, res, next) {
+    if (!req.body.set || req.body.set != "propaganda" || req.body.set != "comments")
+      req.body.set = "comments";
+    
     as.addComment(function () {
-res.redirect('/about/'+req.body.set)
-    }, "comments", req.body)
+    res.redirect('/about/'+req.body.set)
+  }, req.body.set || "comments", req.body)
 });
 
 router.get('/:set?', function (req, res, next) {
-    as.getComments(function (err, all) {
-            for(i of all){
-                delete i.key.digest;
-                delete i.ttl;
-                delete i.gen;
-            }
-            let comments=JSON.stringify(all, null, 4).replace(/\\n/g, '\<br \/\>')
-            res.render('about.html', {settings: settings, comments:comments})
-        })
+  if (!req.body.set || req.body.set != "propaganda" || req.body.set != "comments")
+    req.body.set = "comments";
+  
+  as.getComments(function (err, all) {
+          for(i of all){
+              delete i.key.digest;
+              delete i.ttl;
+              delete i.gen;
+          }
+          let comments=JSON.stringify(all, null, 4).replace(/\\n/g, '\<br \/\>')
+          res.render('about.html', {settings: settings, comments:comments})
+   }, req.body.set || "comments");
 });
 
 
