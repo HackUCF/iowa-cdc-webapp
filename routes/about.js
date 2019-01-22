137b7802de82772223d6785caf643e8df43b52ea
Charlton Trezevant <ct@ctis.me>
2019-01-21 22:58:07 -0500

    Properly fix about.js to prevent arbitrary database access

--- routes/about.js
+++ routes/about.js
@@ -3,27 +3,27 @@ let router = express.Router();
 let as = require('../src/aerospike');
 
 router.post('/', function (req, res, next) {
-    if (!req.body.set || req.body.set != "propaganda" || req.body.set != "comments")
-      req.body.set = "comments";
+  if(!req.body.set || (req.body.set !== "propaganda" && req.body.set !== "comments"))
+    req.body.set = "comments";
     
-    as.addComment(function () {
+  as.addComment(function () {
     res.redirect('/about/'+req.body.set)
-  }, req.body.set || "comments", req.body)
+  }, req.body.set, req.body)
 });
 
 router.get('/:set?', function (req, res, next) {
-  if (!req.body.set || req.body.set != "propaganda" || req.body.set != "comments")
-    req.body.set = "comments";
-  
+  if(!req.params.set || (req.params.set !== "propaganda" && req.params.set !== "comments"))
+    req.params.set = "comments";
+
   as.getComments(function (err, all) {
-          for(i of all){
-              delete i.key.digest;
-              delete i.ttl;
-              delete i.gen;
-          }
-          let comments=JSON.stringify(all, null, 4).replace(/\\n/g, '\<br \/\>')
-          res.render('about.html', {settings: settings, comments:comments})
-   }, req.body.set || "comments");
+      for(i of all){
+          delete i.key.digest;
+          delete i.ttl;
+          delete i.gen;
+      }
+      let comments=JSON.stringify(all, null, 4).replace(/\\n/g, '\<br \/\>')
+      res.render('about.html', {settings: settings, comments:comments})
+  }, req.params.set)
 });
 
 
