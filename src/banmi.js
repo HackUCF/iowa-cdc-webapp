c0febd819268e3ae4eb45b637462b54466339061
Charlton Trezevant <ct@ctis.me>
2019-01-24 01:20:07 -0500

    The last commit message was fake news! This is the true final version of Banmi, packaged up nice and module-like. Bask in her glory!
    This also includes an important behavioral change- now bans will persist 
    until the complete ban time has passed following the last recorded 
    attempt.

--- src/banmi.js
+++ src/banmi.js
@@ -1,66 +1,62 @@
 // banmi.js - 1.0
+// A simple module for rate-limited lockouts
 // (c) 2019 Charlton Trezevant
 // MIT License
 
-// Defaults: Max of 3 attempts within a 5 second window
-var MAX_ATTEMPTS = 3;
-var BAN_LENGTH = 5;
+var Banmi = {};
 
-var bans = {};
+// Defaults: Max of 3 attempts, with a 5 second cool-down period
+Banmi.maxAttempts = 3;
+Banmi.banLength = 5;
 
-function createNewBanRecord(user){
-  bans[user] = {
+Banmi.bans = {};
+
+Banmi.createBanRecord = function(user){
+  this.bans[user] = {
     attempts: 0,
-    firstFailTime: new Date()
+    lastFailTime: new Date()
   };
-}
+};
 
-function banRecordExists(user){
-  return bans[user];
-}
+Banmi.deleteBanRecord = function(user){
+  delete this.bans[user];
+};
 
-function deleteBanRecord(user){
-  delete bans[user];
-}
+Banmi.banRecordExists = function(user){
+  return this.bans[user];
+};
 
-function banTimeElapsed(user){
-  if(!banRecordExists(user))
+Banmi.numAttemptsRecorded = function(user){
+  if(!this.banRecordExists(user))
+    return 0;
+
+  return this.bans[user].attempts;
+};
+
+Banmi.banTimeElapsed = function(user){
+  if(!this.banRecordExists(user))
     return 0;
     
   currentTime = new Date();
-  return Math.floor((currentTime - bans[user].firstFailTime) / 1000);
-}
+  return Math.floor((currentTime - this.bans[user].lastFailTime) / 1000);
+};
 
-function numAttemptsRecorded(user){
-  if(!banRecordExists(user))
-    return 0;
-
-  return bans[user].attempts;
-}
-
-function incrementAttempts(user){
-  if(!banRecordExists(user))
-    createNewBanRecord(user);
+Banmi.recordFailure = function(user){
+  if(!this.banRecordExists(user))
+    this.createBanRecord(user);
   
-  bans[user].attempts++;
-}
+  this.bans[user].lastFailTime = new Date();
+  this.bans[user].attempts++;
+};
 
-function isBanned(user){
-  if(banTimeElapsed(user) >= BAN_LENGTH)
-    deleteBanRecord(user);
+Banmi.isBanned = function(user){
+  if(this.banTimeElapsed(user) >= this.banLength)
+    this.deleteBanRecord(user);
   
-  if(numAttemptsRecorded(user) >= MAX_ATTEMPTS)
+  if(this.numAttemptsRecorded(user) >= this.maxAttempts)
     return true;
   
   return false;
-}
+};
 
-function checkAttempt(user){
-	incrementAttempts(user);
-  return isBanned(user);
-}
-
-module.exports.max_attempts = MAX_ATTEMPTS;
-module.exports.ban_length = BAN_LENGTH;
-module.exports.isBanned = isBanned;
-module.exports.checkAttempt = isBanned;
+module.exports = Banmi;
