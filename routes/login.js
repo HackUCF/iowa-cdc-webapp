36c722543d507b139e8a61cbff8d9d7c30cb0846
Charlton Trezevant <ct@ctis.me>
2019-01-23 21:17:16 -0500

    Add recaptcha support to the login page.

--- routes/login.js
+++ routes/login.js
@@ -2,25 +2,33 @@ var express = require('express');
 var router = express.Router();
 let as = require('../src/aerospike');
 
+var Recaptcha = require('express-recaptcha').Recaptcha;
+recaptcha = new Recaptcha("6LeXUIwUAAAAAMDSKM4DsbEW7V0e05BcA7df1bv7", "6LeXUIwUAAAAAE-J4bfyCM9hw9M4o6a1McdWIGFd", {'theme':'dark'});
 
 router.get('/', function (req, res, next) {
     if (req.cookies.logged_in == "true") {
         res.redirect('/admin')
     } else {
-        res.render('login.html', {settings: settings})
+        res.render('login.html', {settings: settings, captcha:recaptcha.render()})
     }
 });
-router.post('/', function (req, res, next) {
-    as.getUser(req.body.uname, function (result) {
-        if (result.bins.pass == req.body.pass) {
-            res.cookie("logged_in", true);
-            req.cookies.logged_in = true;
-            res.redirect('/admin')
-        } else {
-            res.render('login.html', {settings: settings, failed: true})
-        }
-    });
 
+router.post('/', recaptcha.middleware.verify, function (req, res, next) {
+  recaptcha.verify(req, function(error, data){
+    if (!req.recaptcha.error) {
+      as.getUser(req.body.uname, function (result) {
+          if (result.bins.pass == req.body.pass) {
+              res.cookie("logged_in", true);
+              req.cookies.logged_in = true;
+              res.redirect('/admin')
+          } else {
+              res.render('login.html', {settings: settings, failed: true})
+          }
+      });
+    } else {
+    res.render('login.html', {settings: settings, failed: true});
+    }
+  });
 });
 
 module.exports = router;
