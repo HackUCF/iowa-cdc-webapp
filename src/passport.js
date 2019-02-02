1d51d7a0922f87bad7de4a7ea7e153c712f5ce06
Charlton Trezevant <ct@ctis.me>
2019-02-02 11:37:44 -0600

    Improved logging output from passport.js

--- src/passport.js
+++ src/passport.js
@@ -27,19 +27,23 @@ passport.use(new LocalStrategy({
         clientAddr = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
         return as.getUser(username, (err, user) => {
             if(err){
+                logger.error("User " +  username + " was not found in db from " + clientAddr);
                 banmi.recordFailure(clientAddr);
-                done(err)
+                return done(err, false, {message: 'Incorrect username or password.'});
             }
             if (!user || user.bins === undefined || user.bins.username === undefined){
+                logger.error("User " +  username + " was not found (undefined) in db from " + clientAddr);
                 banmi.recordFailure(clientAddr);
                 return done(null, false, {message: 'Incorrect username or password.'});
             }
             
             bcrypt.compare(env.PASSWORD_SALT + password, user.bins.password).then(function(res) {
               if(res === true){
+                logger.info("User " +  username + ": password match from " + clientAddr);
                 banmi.deleteBanRecord(clientAddr);
                 return done(null, user, {message: 'Logged In Successfully'});
               } else {
+                logger.info("User " +  username + ": password MISmatch from " + clientAddr);
                 banmi.recordFailure(clientAddr);
                 return done(null, false, {message: 'Incorrect username or password.'});
               }
