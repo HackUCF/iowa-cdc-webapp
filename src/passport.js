37831a82c6d6a26e957f91086b68cd37949ff060
Charlton Trezevant <ct@ctis.me>
2019-02-02 03:08:43 -0600

    Passport: banmi now integrated into Local authentication strategy

--- src/passport.js
+++ src/passport.js
@@ -17,22 +17,29 @@ passport.deserializeUser((id, done) => {
 
 passport.use(new LocalStrategy({
         usernameField: 'uname',
-        passwordField: 'pass'
+        passwordField: 'pass',
+        passReqToCallback: true // per https://github.com/jaredhanson/passport-local/blob/master/lib/strategy.js#L26
     },
-    function (username, password, done) {
+    function (req, username, password, done) {
         //this one is typically a DB call. Assume that the returned user object is pre-formatted and ready for storing in JWT
+        clientAddr = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
         return as.getUser(username, (err, user) => {
             if(err){
                 done(err)
             }
-            if (!user || user.bins === undefined || user.bins.username === undefined)
+            if (!user || user.bins === undefined || user.bins.username === undefined){
+                banmi.recordFailure(clientAddr);
                 return done(null, false, {message: 'Incorrect username or password.'});
-                
+            }
+            
             bcrypt.compare(env.PASSWORD_SALT + password, user.bins.password).then(function(res) {
-              if(res === true)
+              if(res === true){
+                banmi.deleteBanRecord(clientAddr);
                 return done(null, user, {message: 'Logged In Successfully'});
-              else
+              } else {
+                banmi.recordFailure(clientAddr);
                 return done(null, false, {message: 'Incorrect username or password.'});
+              }
             });
         });
     }
