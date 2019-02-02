5738cbfd5c96f67721c15a1b6aa467124b69c974
Charlton Trezevant <ct@ctis.me>
2019-02-01 19:39:31 -0600

    Added jwtbuddy for session management

---
+++ src/jwtbuddy.js
@@ -0,0 +1,69 @@
+const jwt = require('jsonwebtoken');
+let as = require('../src/aerospike');
+
+const PRIVILEGE_LEVELS = {
+	"admin": 4,
+	"financier": 3,
+	"developer": 2,
+	"atm": 1,
+	"logged_in": 0,
+	"unauthenticated": -1,
+};
+module.exports.levels = PRIVILEGE_LEVELS;
+
+const LIFESPAN = 5 * 60 * 1000;
+module.exports.issue = function(username, group){
+	return jwt.sign({'sub': username, 'group': group}, env.JWT_SIGNING_KEY, options={expiresIn: LIFESPAN});
+};
+
+var decode = function(jwt){
+	try {
+		let decoded = jwt.verify(jwt, env.JWT_SIGNING_KEY);
+		return decoded;
+	}
+	catch(err) {
+		return {'sub': "", 'group': -1};
+	}
+};
+
+var blacklist = function(jwt, callback){
+	as.addBlacklistedJWT(jwt, function(err){
+		callback(err);
+	});
+};
+
+var isValid = function(jwt, callback){
+	as.checkJWTBlacklist(jwt, function(error, inBlacklist){
+		if(error)
+			callback(false);
+			
+		if(inBlacklist)
+			callback(false);
+		
+		if(decode(jwt).group == -1)
+			callback(false);
+	
+		callback(true);
+	});
+};
+
+var getPrivLevel = function(jwt) {
+	return PRIVILEGE_LEVELS[decode(jwt).group];
+};
+
+module.exports.checkAuthorization = function(requiredLevel){
+	return function(req, res, next){
+		if(req.cookies.session){
+			isValid(req.cookies.session, function(validated){
+				if(validated && getPrivLevel(req.cookies.session) >= requiredLevel)
+					next();
+				else
+					res.sendStatus(403);
+			})
+		} else if (requiredLevel <= 0){
+			next();
+		} else {
+			res.send_status(403);
+		}
+	}
+};
